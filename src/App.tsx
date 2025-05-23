1e91e5f2cca302eb85eae56db58383b6eb765fbb
Exiled Spirit <gab.gabriel.1002@outlook.com>
2025-05-23 17:30:08 -0300

    chore: realiza o scaffolding do projeto em react+swc e adiciona lib de diagrama react-flow com exemplo.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import UseCaseRenderComponent from './components/UseCaseRender'
+
+function App() {
+  return (
+    <>
+      <h1>Title</h1>
+      <UseCaseRenderComponent></UseCaseRenderComponent>
+    </>
+  )
+}
+
+export default App
