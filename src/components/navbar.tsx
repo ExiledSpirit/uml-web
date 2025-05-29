14fee06c64e3146e1acf8a1e634602265fa3023f
Exiled Spirit <gab.gabriel.1002@outlook.com>
2025-05-29 18:25:20 -0300

    feat: tenta aplicar tema tailwind e falha miseravelmente

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 export default function NavbarComponent() {
   return (
-    <div  className="h-12 flex items-center justify-center p-5"> 
-      <h3>UML Use Case</h3>
+    <div  className="h-12 flex items-center justify-center p-5 bg-[var(--color-background)]"> 
+      <h3 className="font-">UML Use Case</h3>
     </div>
   )
 }
